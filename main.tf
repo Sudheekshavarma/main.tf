a6be0ba3beff1d7ff2f387413e6a5512fee0a75d
Sudheekshavarma <lakkarasu.sudheeksha@dxc.com>
2025-04-22 13:52:54 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -3,9 +3,25 @@ provider "google" {
  region  = "us-central1"
  credentials = "keys.json"
 }
-data "google_compute_network" "default_network" {
-  name = "default"
+
+resource "google_compute_network" "vpc_network1" {
+ name                    = "vpc-network"
 }
+
+resource "google_compute_network" "vpc_network2" {
+ name                    = "dxc-network"
+}
+data "google_compute_network" "datasource_vpc_network2" {
+  name = google_compute_network.vpc_network2.name
+}
+
+resource "google_compute_subnetwork" "vpc_subnet" {
+ name          = "vpc-subnet"
+ ip_cidr_range = "10.0.0.0/24"
+ network       = google_compute_network.vpc_network1.id
+ region        = "us-central1"
+}
+ 
 
 resource "google_compute_instance" "my_instance" {
  name         = "my-instance"
@@ -18,8 +34,12 @@ resource "google_compute_instance" "my_instance" {
     }
 }
  network_interface {
-   network = data.google_compute_network.default_network.id
+   subnetwork = google_compute_subnetwork.vpc_subnet.id
    access_config {}
    }
-
+ network_interface {
+   network = data.google_compute_network.datasource_vpc_network2.id
+   access_config {}
+     
+ }
 }
