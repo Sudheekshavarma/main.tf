0b2a4ef4c4d87ca63e076bec3ce39215359e0fb8
Sudheekshavarma <lakkarasu.sudheeksha@dxc.com>
2025-04-22 17:35:57 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -3,43 +3,22 @@ provider "google" {
  region  = "us-central1"
  credentials = "keys.json"
 }
-
-resource "google_compute_network" "vpc_network1" {
- name                    = "vpc-network"
-}
-
-resource "google_compute_network" "vpc_network2" {
- name                    = "dxc-network"
-}
-data "google_compute_network" "datasource_vpc_network2" {
-  name = google_compute_network.vpc_network2.name
-}
-
-resource "google_compute_subnetwork" "vpc_subnet" {
- name          = "vpc-subnet"
- ip_cidr_range = "10.0.0.0/24"
- network       = google_compute_network.vpc_network1.id
- region        = "us-central1"
-}
- 
-
 resource "google_compute_instance" "my_instance" {
- name         = "my-instance"
- machine_type = "e2-micro"
- zone         = "us-central1-a"
+  count        = 4
+  name         = "terraform-${count.index}"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
  
- boot_disk {
-   initialize_params {
-     image = "debian-cloud/debian-11"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
     }
-}
- network_interface {
-   subnetwork = google_compute_subnetwork.vpc_subnet.id
-   access_config {}
-   }
- network_interface {
-   network = data.google_compute_network.datasource_vpc_network2.id
-   access_config {}
-     
- }
+  }
+ 
+  network_interface {
+    network = "default"
+    access_config {}    
+  }
+ 
+ 
 }
